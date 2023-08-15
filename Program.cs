e604d54801cacdae3021accdcdd27a0f1c3746bd
ddx1292680719 <1292680719@qq.com>
2023-08-15 23:04:04 +0800

    只显示CPu 位数

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Reflection;
+using System.Reflection.PortableExecutable;
 using System.Runtime.InteropServices;
 using System.Runtime.Loader;
 
@@ -7,8 +8,9 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        string directoryPath = @"C:\Path\To\Directory";
+        // System.Console.WriteLine(args[0]);
+        // Console.WriteLine("Hello, World!");
+        string directoryPath = args[0];
 
         string[] assemblyFiles = Directory.GetFiles(directoryPath, "*.dll");
 
@@ -16,22 +18,25 @@ internal class Program
         {
             try
             {
-                AssemblyLoadContext context = new AssemblyLoadContext(null, true);
-                Assembly assembly = context.LoadFromAssemblyPath(assemblyFile);
-                CustomAttributeData targetPlatformAttribute = GetTargetPlatformAttribute(assembly);
+                System.Console.WriteLine($"cpu machine : {GetTargetCpuArch(assemblyFile)}");
+                // AssemblyLoadContext context = new AssemblyLoadContext(null, true);
+                // Assembly assembly = context.LoadFromAssemblyPath(assemblyFile);
+                // CustomAttributeData targetPlatformAttribute = GetTargetPlatformAttribute(assembly);
+                // AssemblyName assemblyName = AssemblyName.GetAssemblyName(assemblyFile);
 
-                if (targetPlatformAttribute != null)
-                {
-                    Console.WriteLine($"Assembly: {assembly.FullName}");
-                    Console.WriteLine($"Target Platform: {targetPlatformAttribute.ConstructorArguments[0].Value}");
-                    Console.WriteLine();
-                }
-                else
-                {
-                    Console.WriteLine($"not get the Target Platform into from assembly : {assembly.FullName}");
-                }
+                // if (targetPlatformAttribute != null)
+                // {
+                //     Console.WriteLine($"Assembly: {assembly.FullName}");
+                //     Console.WriteLine($"Target Platform: {targetPlatformAttribute.ConstructorArguments[0].Value}");
+                //     System.Console.WriteLine($"cpu machine : {GetTargetCpuArch(assemblyFile)}");
+                //     Console.WriteLine();
+                // }
+                // else
+                // {
+                //     Console.WriteLine($"not get the Target Platform into from assembly : {assembly.FullName}");
+                // }
 
-                context.Unload();
+                // context.Unload();
             }
             catch (Exception ex)
             {
@@ -49,5 +54,21 @@ internal class Program
             }
         }
         return null;
+    }
+
+    static string GetTargetCpuArch(string assemblyPath)
+    {
+        using (var stream = System.IO.File.OpenRead(assemblyPath))
+        using (var peReader = new PEReader(stream))
+        {
+            // Get the COFF header of the assembly
+            CoffHeader coffHeader = peReader.PEHeaders.CoffHeader;
+
+            // Get the target CPU architecture
+            Machine targetMachine = coffHeader.Machine;
+            return targetMachine.ToString();
+            // Print the target CPU architecture
+            Console.WriteLine("Target CPU architecture: " + targetMachine);
+        }
     }
 }
