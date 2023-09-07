9fb018e2949eb985b170e2541c9c0aa25d957bf0
ddx1292680719 <1292680719@qq.com>
2023-09-07 22:20:01 +0800

    把主程序移动到Main文件夹中并创建解决方案

---
+++ Main/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices;
+using System.Runtime.Loader;
+using MyAssemblyDependency;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            AssemblyDependencyGetter.ShowInfo();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error loading assembly : {ex.Message}");
+        }
+    }
+
+}
