6ce1c635bdc96846011b325fe84530349103101d
ddx1292680719 <1292680719@qq.com>
2023-09-07 22:12:49 +0800

    从Main中调用实验性的AssemblyDependencyGetter.ShowInfo方法，显示

--- Program.cs
+++ Program.cs
@@ -3,72 +3,20 @@ using System.Reflection;
 using System.Reflection.PortableExecutable;
 using System.Runtime.InteropServices;
 using System.Runtime.Loader;
+using MyAssemblyDependency;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        // System.Console.WriteLine(args[0]);
-        // Console.WriteLine("Hello, World!");
-        string directoryPath = args[0];
-
-        string[] assemblyFiles = Directory.GetFiles(directoryPath, "*.dll");
-
-        foreach (string assemblyFile in assemblyFiles)
+        try
         {
-            try
-            {
-                System.Console.WriteLine($"cpu machine : {GetTargetCpuArch(assemblyFile)}");
-                // AssemblyLoadContext context = new AssemblyLoadContext(null, true);
-                // Assembly assembly = context.LoadFromAssemblyPath(assemblyFile);
-                // CustomAttributeData targetPlatformAttribute = GetTargetPlatformAttribute(assembly);
-                // AssemblyName assemblyName = AssemblyName.GetAssemblyName(assemblyFile);
-
-                // if (targetPlatformAttribute != null)
-                // {
-                //     Console.WriteLine($"Assembly: {assembly.FullName}");
-                //     Console.WriteLine($"Target Platform: {targetPlatformAttribute.ConstructorArguments[0].Value}");
-                //     System.Console.WriteLine($"cpu machine : {GetTargetCpuArch(assemblyFile)}");
-                //     Console.WriteLine();
-                // }
-                // else
-                // {
-                //     Console.WriteLine($"not get the Target Platform into from assembly : {assembly.FullName}");
-                // }
-
-                // context.Unload();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error loading assembly {assemblyFile}: {ex.Message}");
-            }
+            AssemblyDependencyGetter.ShowInfo();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error loading assembly : {ex.Message}");
         }
     }
-    static CustomAttributeData GetTargetPlatformAttribute(Assembly assembly)
-    {
-        foreach (CustomAttributeData attribute in assembly.GetCustomAttributesData())
-        {
-            if (attribute.AttributeType.FullName == "System.Runtime.Versioning.TargetFrameworkAttribute")
-            {
-                return attribute;
-            }
-        }
-        return null;
-    }
 
-    static string GetTargetCpuArch(string assemblyPath)
-    {
-        using (var stream = System.IO.File.OpenRead(assemblyPath))
-        using (var peReader = new PEReader(stream))
-        {
-            // Get the COFF header of the assembly
-            CoffHeader coffHeader = peReader.PEHeaders.CoffHeader;
-
-            // Get the target CPU architecture
-            Machine targetMachine = coffHeader.Machine;
-            return targetMachine.ToString();
-            // Print the target CPU architecture
-            Console.WriteLine("Target CPU architecture: " + targetMachine);
-        }
-    }
 }
